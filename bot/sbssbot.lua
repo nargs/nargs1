88ae9a4ecd2af221eecff080412b83e601db00e5
nargs <nargs001@gmail.com>
2017-01-30 02:10:21 +0330

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -239,7 +239,7 @@ function create_config( )
     "time",
     "welcome"
   }.	
-    sudo_users = {310896266.310896266},--Sudo users
+    sudo_users = {300882227.300882227.300882227},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V2 Beta
